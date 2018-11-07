801046e5ee1d394e01df3d4589ca0aca93437051
orlandoxds <34320335+orlandoxds@users.noreply.github.com>
2018-11-06 16:18:35 -0800

    Initial commit

---
+++ app/src/main/java/com/example/user/p8runtimefragment/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.user.p8runtimefragment;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+   public static FragmentManager fragmentManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fragmentManager=getSupportFragmentManager();
+        if(savedInstanceState!=null){
+            return;
+        }
+        FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+        HomeFragment homeFragment = new HomeFragment();
+        fragmentTransaction.add(R.id.fragment_container,homeFragment, null);
+        fragmentTransaction.commit();
+
+    }
+}
